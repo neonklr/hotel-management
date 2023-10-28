7544b2ab73281d20b9957d1d1f507531613dbae5
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-10-28 14:51:53 +0530

    implented email services

---
+++ emailservices/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.http import HttpResponse
+from django.conf import settings
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def send_email(request):
+    subject = 'Booking Details'
+    message = f'Thank You for booking at our hotel.'
+    from_email = settings.EMAIL_HOST_USER
+    recipient_list = ['ujjwalj12222@gmail.com']
+
+    send_mail(subject, message, from_email, recipient_list)
+
+    return HttpResponse("Email sent")

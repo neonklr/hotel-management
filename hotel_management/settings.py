a30d394da2e280de320446d48fd7f9d3a289b0a6
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-10-28 14:48:50 +0530

    implemented email service using django server

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "emailservices"
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'raajvsingh2@gmail.com'
+EMAIL_HOST_PASSWORD = 'xwvo nvys rgcj zpeg'

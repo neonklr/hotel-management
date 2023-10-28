21868fb77dea58d07e6f97f9197ef29fa272b605
neonklr <neonklr@users.noreply.github.com>
2023-10-28 19:29:34 +0000

    Code quality enforced [Enforcer Bot]

--- reservation/migrations/0002_remove_reservation_checked_out_and_more.py
+++ reservation/migrations/0002_remove_reservation_checked_out_and_more.py
@@ -4,19 +4,18 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('reservation', '0001_initial'),
+        ("reservation", "0001_initial"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='reservation',
-            name='checked_out',
+            model_name="reservation",
+            name="checked_out",
         ),
         migrations.AddField(
-            model_name='reservation',
-            name='checked_out_at',
+            model_name="reservation",
+            name="checked_out_at",
             field=models.DateTimeField(null=True),
         ),
     ]

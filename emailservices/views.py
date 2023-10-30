577b2d41a1ee6038bab771982e1ec94ba64dd503
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-10-30 23:21:53 +0530

    improved code quality in emailservices

--- emailservices/views.py
+++ emailservices/views.py
@@ -4,17 +4,19 @@ from django.conf import settings
 
 # Create your views here.
 
-
-def home(request):
-    return render(request, "index.html")
-
-
 def send_email(request):
     subject = "Booking Details"
     message = "Thank You for booking at our hotel. \nYour total paid amount is ---"
     from_email = settings.EMAIL_HOST_USER
-    recipient_list = ["ujjwalj12222@gmail.com"]
+    recipient_list = ["ujjwalj1222@gmail.com"]
 
     send_mail(subject, message, from_email, recipient_list)
 
     return render(request, "Thankyou.html")
+
+
+# def home(request):
+#     return render(request, "index.html")
+
+
+

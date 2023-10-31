9859838c9f315f46429f8e885594fbfa0a71ec37
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-10-31 12:57:15 +0530

    created cancellation email notification

--- emailservices/urls.py
+++ emailservices/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("send_email/", views.send_email, name="send_email"),
+    path('earlycancel/', views.cancel_res, name = 'cancel_refund'),
+    path('earlycancel/cancel/', views.send_refund_email, name = 'cancel_refund_mail'),
+
 ]

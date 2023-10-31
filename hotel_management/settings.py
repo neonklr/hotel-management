3d1b6a2067096c72c9370f52f8fe3c14db62332b
Rishaab Kalra <55053464+PeaPals@users.noreply.github.com>
2023-10-31 22:36:18 +0530

    minor integration changes

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     "get_started",
     "user_profile",
     "dashboard",
-    "dashboard",
 ]
 
 MIDDLEWARE = [
--- hotel_management/urls.py
+++ hotel_management/urls.py
@@ -18,14 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("home.urls"), name="home"),
-    path("dashboard/", include("dashboard.urls"), name="dashboard"),
-]
-urlpatterns = [
     path("", include("home.urls"), name="home"),
     path("admin/", admin.site.urls),
     path("get_started/", include("get_started.urls"), name="get_started"),
     path("auth/", include("authentication.urls"), name="auth"),
     path("user/", include("user_profile.urls"), name="user_profile"),
+    path("dashboard/", include("dashboard.urls"), name="dashboard"),
 ]

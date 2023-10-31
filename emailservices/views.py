f1f4716eaaf98ecdd123aafb4376448beff5ca1f
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-10-31 13:31:16 +0530

    completed refund email notification

--- emailservices/views.py
+++ emailservices/views.py
@@ -10,7 +10,7 @@ def send_email(request):
     subject = "Booking Confirmed"
     message = "Thank You for booking at our hotel. \nYour total paid amount is ---"
     from_email = settings.EMAIL_HOST_USER
-    recipient_list = ["ujjwalj12222"]
+    recipient_list = ["ujjwalj12222@gmail.com"]
 
     try:
         send_mail(subject, message, from_email, recipient_list)
@@ -46,7 +46,7 @@ def send_refund_email(request):
     subject = "Booking Cancelled"
     message = f"<strong>We wanted to confirm that we have received your request to cancel your reservation with us. We understand that sometimes plans change, and we're here to assist you with this process.<br><br>Here are the details of your canceled reservation: <br>Reservation ID: [Reservation ID]<br>Customer Name: {customer_name}<br>Reservation Date: {booking_till_str}<br>Cancellation Date: {checkout_date_str}<br>Refund Amount: {refundAmount}<br><br>If you have any questions or require any further assistance regarding your cancellation or potential rebooking, please don't hesitate to contact our customer service team at [Customer Service Email] or [Customer Service Phone Number]. We're here to help and ensure your experience with us is as smooth as possible.Thank you for considering our services, and we hope to have the opportunity to serve you in the future. We value your business and appreciate your understanding.<br><br>Warm regards,<br>Prithvi Raj Chauhan<br>Email Services Department<br>Bloom Stays<br>+91 111 222 0000"
     from_email = settings.EMAIL_HOST_USER
-    recipient_list = ["ujjwalj12222"]
+    recipient_list = ["ujjwalj12222@gmail.com"]
 
     try:
         send_mail(subject, "", from_email, recipient_list, html_message=message)

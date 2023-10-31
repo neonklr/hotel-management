1c5e4b3489cbdafecaf59a0ea39f01eb4115aa42
Rishaab Kalra <55053464+PeaPals@users.noreply.github.com>
2023-10-31 22:06:01 +0530

    minor changes

--- reservation/url.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("check_availability/", views.check_availability, name="check_availability"),  # type:ignore
-]
--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,6 @@
 # Register your models here.
+from django.contrib import admin
+
+from users.models import User
+
+admin.site.register(User)

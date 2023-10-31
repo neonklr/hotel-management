7afbb41b8f632652d84c37f885785ba92ff3aaf1
BalajiSatrusalya <6balaji9@gmail.com>
2023-10-31 12:12:29 +0530

    added simple update profile logic

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,11 +1,15 @@
 # Create your views here.
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from users.models import User
 
 def update_profile(request, user=None):
-    if user:
+    if request.method  == "GET":
+        # if user:
         return render(request, "update.html", {"user": user})
+        # return HttpResponse("<h3>direct access forbidden</h3>")
     
-    return HttpResponse("<h3>direct access forbidden</h3>")
+    elif request.method == "POST":
+        return update_profile_logic(request)
 
 
 def view_profile(request, user=None):
@@ -13,3 +17,21 @@ def view_profile(request, user=None):
         return render(request, "view.html", {"user": user})
     
     return HttpResponse("<h3>direct access forbidden</h3>")
+
+
+def update_profile_logic(request):
+    data = {
+        "name": request.POST["name"],
+        "email" : request.POST["email"],
+        "password" : request.POST["password"],
+        "date_of_birth" : request.POST["DateOfBirth"],
+        "phone_number" : request.POST["phoneNumber"],
+        "address" : request.POST["Address"]
+    }
+
+    for key, value in data.items():
+        if not value:
+            return redirect("/user/update")
+
+    User(**data).save()
+    return redirect("/dashboard")

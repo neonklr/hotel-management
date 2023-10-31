c214f31ad91c6ef2990b93c3c3159fa36f584b4f
ujjwaljamuar <ujjwaljamuar@users.noreply.github.com>
2023-10-31 07:28:25 +0000

    Code quality enforced [Enforcer Bot]

--- emailservices/urls.py
+++ emailservices/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("send_email/", views.send_email, name="send_email"),
-    path('earlycancel/', views.cancel_res, name = 'cancel_refund'),
-    path('earlycancel/cancel/', views.send_refund_email, name = 'cancel_refund_mail'),
-
+    path("earlycancel/", views.cancel_res, name="cancel_refund"),
+    path("earlycancel/cancel/", views.send_refund_email, name="cancel_refund_mail"),
 ]
--- emailservices/views.py
+++ emailservices/views.py
@@ -4,10 +4,10 @@ from django.core.mail import send_mail
 from django.conf import settings
 from datetime import datetime
 
-# Create your views here 
+# Create your views here
+
 
 def send_email(request):
-  
     subject = "Booking Confirmed"
     message = "Thank You for booking at our hotel. \nYour total paid amount is ---"
     from_email = settings.EMAIL_HOST_USER
@@ -15,31 +15,32 @@ def send_email(request):
 
     try:
         send_mail(subject, message, from_email, recipient_list)
-        print('email sent')
+        print("email sent")
 
         return render(request, "Thankyou.html")
-    
+
     except:
-        return HttpResponse('<h1>Something went wrong!</h1>')
+        return HttpResponse("<h1>Something went wrong!</h1>")
+
 
 def cancel_res(request):
-    return render(request, 'EarlyCancelModal.html')
+    return render(request, "EarlyCancelModal.html")
+
 
 def send_refund_email(request):
-    
     perDayPrice = 2000
-    booking_till_str = '2023-11-05'
+    booking_till_str = "2023-11-05"
     checkout_date_str = str(datetime.today().date())
-    
-    booking_till = datetime.strptime(booking_till_str, '%Y-%m-%d')
-    checkout_date = datetime.strptime(checkout_date_str, '%Y-%m-%d')
+
+    booking_till = datetime.strptime(booking_till_str, "%Y-%m-%d")
+    checkout_date = datetime.strptime(checkout_date_str, "%Y-%m-%d")
 
     date_difference = booking_till - checkout_date
     daysleft = date_difference.days
     # paidAmount = ''
-    refundAmount = daysleft*perDayPrice
+    refundAmount = daysleft * perDayPrice
 
-    customer_name = 'Ujjwal Jamuar'
+    customer_name = "Ujjwal Jamuar"
 
     subject = "Booking Cancelled"
     message = f"<strong>We wanted to confirm that we have received your request to cancel your reservation with us. We understand that sometimes plans change, and we're here to assist you with this process.<br><br>Here are the details of your canceled reservation: <br>Reservation ID: [Reservation ID]<br>Customer Name: {customer_name}<br>Reservation Date: {booking_till_str}<br>Cancellation Date: {checkout_date_str}<br>Refund Amount: {refundAmount}<br><br>If you have any questions or require any further assistance regarding your cancellation or potential rebooking, please don't hesitate to contact our customer service team at [Customer Service Email] or [Customer Service Phone Number]. We're here to help and ensure your experience with us is as smooth as possible.Thank you for considering our services, and we hope to have the opportunity to serve you in the future. We value your business and appreciate your understanding.<br><br>Warm regards,<br>Prithvi Raj Chauhan<br>Email Services Department<br>Bloom Stays<br>+91 111 222 0000"
@@ -48,13 +49,12 @@ def send_refund_email(request):
 
     try:
         send_mail(subject, "", from_email, recipient_list, html_message=message)
-        print('email sent')
+        print("email sent")
 
         return render(request, "Thankyou.html")
 
     except:
-        return HttpResponse('<h1>Something went wrong!</h1>')
-
+        return HttpResponse("<h1>Something went wrong!</h1>")
 
 
 # def home(request):

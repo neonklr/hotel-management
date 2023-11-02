dc48384dc1d3ae2cf862f4f1eb838a2f4fef0e60
Ahisha2001 <ahisha.roy2001@gmail.com>
2023-11-02 13:41:00 +0530

    Logic for new reservations

--- reservation/views.py
+++ reservation/views.py
@@ -1,5 +1,10 @@
-# Create your views here.
-from django.shortcuts import render
+# # Create your views here.
+from django.contrib import messages
+from django.shortcuts import HttpResponse, redirect, render
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
+from users.models import User
+from .models import Reservation, Room
 
 
 def new_reservation_view(request):
@@ -8,3 +13,85 @@ def new_reservation_view(request):
 
 def update_reservation_view(request):
     return render(request, "update.html")
+
+
+@login_required
+def cancel_reservation(request, uuid):
+    # Cancels reservations iff user is logged in and validated.
+    resv = Reservation.objects.get(uuid=uuid)
+    print(resv.email)
+    print(User.objects.get(email=request.session.get("login_token")))
+    if resv.email != User.objects.get(email=request.session.get("login_token")):
+        messages.error(request, "You are not authorized to cancel this reservation.")
+        return redirect("/")
+
+    resv.completed = "Cancelled"
+    resv.room_no.is_available = True
+    resv.room_no.save()
+    resv.save()
+    messages.success(request, "Reservation canceled successfully.")
+    return redirect("/dashboard/")
+
+
+def room_list(request):
+    if request.method == "GET":
+        return render(request, "new.html")
+    elif request.method == "POST":
+        start_time_str = request.POST.get("checkIn")
+        end_time_str = request.POST.get("checkOut")
+        start_time = datetime.strptime(start_time_str, "%Y-%m-%d")
+        end_time = datetime.strptime(end_time_str, "%Y-%m-%d")
+        available_rooms_count = calculate_available_rooms_by_room_type(start_time, end_time)
+        return render(
+            request,
+            "new.html",
+            {"room_type_counts": available_rooms_count, "checkIn": start_time_str, "checkOut": end_time_str},
+        )
+
+
+def calculate_available_rooms_by_room_type(start_time, end_time):
+    available_rooms_count = {}
+    room_types = Room.objects.values("room_type").distinct()
+    for room_type in room_types:
+        room_type_name = room_type["room_type"]
+        rooms_of_type = Room.objects.filter(room_type=room_type_name)
+        available_count = 0
+        for room in rooms_of_type:
+            reservations = Reservation.objects.filter(
+                room_no=room, booked_from__lte=end_time, booked_to__gte=start_time
+            )
+            if not reservations.exists():
+                available_count += 1
+        available_rooms_count[room_type_name] = available_count
+    return available_rooms_count
+
+# Logic for booking rooms
+def book_rooms(request):
+    # room = Room.objects.get(pk="room_no")
+    if request.method == "POST":
+        room_type = request.POST.get("roomType")
+        start_time_str = request.POST.get("checkIn")
+        end_time_str = request.POST.get("checkOut")
+        print(room_type)
+        available_rooms = Room.objects.filter(is_available=True, room_type=room_type)
+        room = available_rooms[0]
+        no_of_days = (datetime.strptime(end_time_str, "%Y-%m-%d") - datetime.strptime(start_time_str, "%Y-%m-%d")).days
+        if start_time_str and end_time_str:
+            resv = Reservation(
+                date=datetime.now(),
+                # email=User(request.session.get("login_token")),
+                email=User.objects.get(email=request.session.get("login_token")),
+                booked_from=start_time_str,
+                booked_to=end_time_str,
+                room_no=room,
+                payment_method="Cash",
+                payment_amount=no_of_days * room.room_price,
+                completed="No",
+            )
+            resv.save()
+            room.is_available = False
+            room.save()
+            return redirect("/dashboard")
+        else:
+            return HttpResponse("Please fill in all the fields.")
+    return render(request, "reservation/new.html")
--- rooms/models.py
+++ rooms/models.py
@@ -6,4 +6,7 @@ class Room(models.Model):
     room_no = models.IntegerField(primary_key=True, unique=True)
     room_type = models.CharField(max_length=100)
     room_price = models.IntegerField()
+    # booked_from=models.DateTimeField(null=True,blank=True)
+    # booked_to=models.DateTimeField(null=True,blank=True)
     is_available = models.BooleanField(default=True)
+

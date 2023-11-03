c1c48a644945920694724af2bee301ed77192836
aanchal-05 <aanchalmanchanda04@gmail.com>
2023-11-03 09:17:50 +0530

    encryption added

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,8 +2,20 @@
 
 from django.shortcuts import render
 from authentication.logic import auth
+from users.models import User
+from authentication.logic import get_session_data
 
+#def profilename(request):
+   
+       # user = User.objects.get(email=email)
 
 @auth()
 def index(request):
-    return render(request, "dashboard/dashboard.html")
+    user=get_session_data(request, "login_token")
+    split_value=user.split("@")
+    firstname=split_value[0]
+    print(firstname,"user")
+    # d={
+    #     firstname
+    # }
+    return render(request, "dashboard/dashboard.html", {'name':firstname} )

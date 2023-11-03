6f00c772226d019d0b965180b45b07b31bea9541
aanchal-05 <aanchal-05@users.noreply.github.com>
2023-11-03 03:48:48 +0000

    Code quality enforced [Enforcer Bot]

--- authentication/views.py
+++ authentication/views.py
@@ -6,55 +6,58 @@ from django.shortcuts import redirect
 from users.models import User
 
 from . import logic
-import bcrypt
 
 import hashlib
 import os
+
 
 # Function to hash passwords using SHA-256
 def hash_password(password, salt=None):
     if salt is None:
         salt = os.urandom(16)  # Generate a random salt if not provided
-        
+
     # Hash the password using SHA-256 and the salt
     hasher = hashlib.sha256()
     hasher.update(salt)
-    hasher.update(password.encode('utf-8'))
+    hasher.update(password.encode("utf-8"))
     hashed_password = hasher.digest()
-    
+
     # Return the salt and hashed password
     return hashed_password
+
 
 # Function to hash passwords using SHA-256
 def hashpassword(password, salt=None):
     if salt is None:
         salt = os.urandom(16)  # Generate a random salt if not provided
-        
+
     # Hash the password using SHA-256 and the salt
     hasher = hashlib.sha256()
     hasher.update(salt)
-    hasher.update(password.encode('utf-8'))
+    hasher.update(password.encode("utf-8"))
     hashed_password = hasher.digest()
-    
+
     # Return the salt and hashed password
-    return hashed_password,salt
+    return hashed_password, salt
+
 
 # Function to verify passwords
 def verify_password(entered_password, stored_salt, stored_hashed_password):
     # Verify the entered password using the stored salt and hashed password
     hasher = hashlib.sha256()
     hasher.update(stored_salt)
-    hasher.update(entered_password.encode('utf-8'))
+    hasher.update(entered_password.encode("utf-8"))
     hashed_password = hasher.digest()
-    
+
     # Compare the computed hash with the stored hash
     return hashed_password == stored_hashed_password
 
+
 # Example usage
-#plain_password = "user_input_password"
+# plain_password = "user_input_password"
 
 # Hash the password and get the salt and hashed password
-#salt, hashed_password = hash_password(plain_password)
+# salt, hashed_password = hash_password(plain_password)
 
 # Store the salt and hashed_password in your database
 # ...
@@ -65,6 +68,7 @@ def verify_password(entered_password, stored_salt, stored_hashed_password):
 #     print("Authentication successful")
 # else:
 #     print("Authentication failed")
+
 
 def signup(request):
     if logic.get_session_data(request, "login_token"):
@@ -86,7 +90,7 @@ def signup(request):
         # hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         # print(hashed_password,"pass")
 
-        print(hash_password(password),"pass")
+        print(hash_password(password), "pass")
         User(email=email, password=hash_password(password)).save()
         logic.set_session_data(request, "login_token", email)
         messages.success(request, "Logged in successfully")
@@ -101,17 +105,17 @@ def login(request):
         email = request.POST["email"]
         password = request.POST["password"]
 
-        #user = User.objects.filter(email=email).filter(password=password).first()
+        # user = User.objects.filter(email=email).filter(password=password).first()
         user = User.objects.get(email=email)
 
         stored_hashed_password = user.password
-        hashpass,salt=hashpassword(password)
-        if user and verify_password(password,salt,hashpass) :
-        #bcrypt.checkpw(password.encode('utf-8'), stored_hashed_password.encode('utf-8')):
-        #if bcrypt.checkpw(entered_password.encode('utf-8'), stored_hashed_password):
+        hashpass, salt = hashpassword(password)
+        if user and verify_password(password, salt, hashpass):
+            # bcrypt.checkpw(password.encode('utf-8'), stored_hashed_password.encode('utf-8')):
+            # if bcrypt.checkpw(entered_password.encode('utf-8'), stored_hashed_password):
 
-       # if user.email and bcrypt.checkpw(password.encode('utf8'), user.password):
-        # if user:
+            # if user.email and bcrypt.checkpw(password.encode('utf8'), user.password):
+            # if user:
             logic.set_session_data(request, "login_token", email)
             messages.success(request, "Logged in successfully")
             return redirect("/dashboard")
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,20 +2,20 @@
 
 from django.shortcuts import render
 from authentication.logic import auth
-from users.models import User
 from authentication.logic import get_session_data
 
-#def profilename(request):
-   
-       # user = User.objects.get(email=email)
+# def profilename(request):
+
+# user = User.objects.get(email=email)
+
 
 @auth()
 def index(request):
-    user=get_session_data(request, "login_token")
-    split_value=user.split("@")
-    firstname=split_value[0]
-    print(firstname,"user")
+    user = get_session_data(request, "login_token")
+    split_value = user.split("@")
+    firstname = split_value[0]
+    print(firstname, "user")
     # d={
     #     firstname
     # }
-    return render(request, "dashboard/dashboard.html", {'name':firstname} )
+    return render(request, "dashboard/dashboard.html", {"name": firstname})
